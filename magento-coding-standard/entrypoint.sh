fa1e60a7561531ad7147d1a4909deccf9f59f4b4
Tran Quy Hiep <tranquyhiep@sutunam.com>
2020-12-08 19:16:58 +0700

    Update code

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -7,4 +7,5 @@ echo "::add-matcher::${HOME}/problem-matcher.json"
 
 cd $GITHUB_WORKSPACE
 test -z "${INPUT_PHPCS_STANDARD}" && INPUT_PHPCS_STANDARD=Magento2
-sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE/app/code -s $*"
+test -z "${INPUT_PHPCS_IGNORE}" && INPUT_PHPCS_IGNORE=*/vendor/*
+sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE/app --ignore=$INPUT_PHPCS_IGNORE -s $*"
